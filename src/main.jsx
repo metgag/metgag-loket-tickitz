c811c409b0f6664e8d1f447a9cc9242bf8a45bbd
metgag <moreehigh1@gmail.com>
2025-07-30 00:47:30 +0700

    refactor: mv tailwind and nerdfonts import to internal

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Register from './pages/auth/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
