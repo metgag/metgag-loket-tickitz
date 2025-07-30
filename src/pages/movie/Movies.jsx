76c0368589636f951b4e4f1eabd681dd198cf47a
metgag <moreehigh1@gmail.com>
2025-07-30 07:44:09 +0700

    feat: add movies page and few style for page, Navbar, and Footer

---
+++ src/pages/movie/Movies.jsx
@@ -0,0 +1,124 @@
+import { Fragment, useState, useEffect } from 'react'
+import './global.css'
+import './style.css'
+import Navbar from '../../components/Navbar.jsx'
+import Footer from '../../components/Footer.jsx'
+
+export default function Movies() {
+  const [movies, setMovies] = useState([]);
+
+  const apiToken = import.meta.env.VITE_API_TOKEN;
+  const urlMovies = import.meta.env.VITE_MOVIES_URL;
+  const urlGenres = import.meta.env.VITE_GENRES_URL;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${apiToken}`
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const promises = [fetch(urlMovies, options), fetch(urlGenres, options)];
+      const [moviesResp, genresResp] = await Promise.all(promises);
+      const { results: movieResults } = await moviesResp.json();
+      const { genres } = await genresResp.json();
+      const genresMap = new Map();
+      genres.forEach((genre) => {
+        genresMap.set(genre.id, genre.name);
+      });
+
+      const movies = movieResults.map((movie) => {
+        const { id, title, genre_ids, poster_path } = movie;
+        const result = {
+          id,
+          title,
+          poster_path,
+        };
+        const genres = genre_ids.map((genreId) => {
+          return genresMap.get(genreId);
+        });
+        Object.assign(result, { genres });
+        return result;
+      });
+
+      setMovies(movies);
+    })();
+  }, []);
+
+  return (
+    <Fragment>
+      <Navbar />
+      <main className="d-flex flex-col">
+        <div className="d-flex flex-col">
+          <section id="hero-bg" className="d-flex flex-col justify-center">
+            {/* <!-- <div class="hero-bg"></div> --> */}
+            <h4>LIST MOVIE OF THE WEEK</h4>
+            <h1>Experience the Magic of Cinema: Book Your Tickets Today</h1>
+            <div id="pg-num" className="d-flex flex-row">
+              <div className="pg pg-1"></div>
+              <div className="pg pg-2"></div>
+              <div className="pg pg-3"></div>
+            </div>
+          </section>
+
+          <section id="event" className="d-flex">
+            <form action="">
+              <label htmlFor="f-event">Cari Event</label>
+              <input type="text" name="" id="f-event"
+                placeholder="New Born Expert" />
+            </form>
+
+            <div id="filter">
+              <label htmlFor="">Filter</label>
+            </div>
+          </section>
+
+          <section id="watch-today" className="d-flex flex-col align-center">
+            <div className="movies-grid">
+              {/* <div className="thumbnail thumbnail-1">
+                <img src="/assets/images/thumbnail/thumb-bl-widow.png"
+                  alt="" />
+                <h4>Black Widow</h4>
+                <div className="genre">
+                  <p>Action</p>
+                  <p>Adventure</p>
+                </div>
+              </div> */}
+              {movies.length > 0 && movies.map((movie) => {
+                console.log(movie);
+                return (
+                  <div key={movie.id} className={`thumbnail-${movie.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+                    <h4>{movie.title}</h4>
+                    <div className="genre">
+                      {movie.genres.map((genre, id) => {
+                        return <p>{genre}</p>
+                      })}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            <div id="pg-nav" className="d-flex">
+              <button>1</button><button>2</button><button>3</button><button>4</button>
+              <button><i className="nf nf-oct-arrow_right"></i></button>
+            </div>
+          </section>
+
+          <section className="bg-blue d-flex flex-col align-center" id="newsletter">
+            <h1>Subscribe to our newsletter</h1>
+            <form className="d-flex" action="">
+              <input type="text" placeholder="First name" />
+              <input type="email" placeholder="Email address" />
+              <button>Subscribe Now</button>
+            </form>
+            <div className="circle"></div>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </Fragment>
+  )
+}
