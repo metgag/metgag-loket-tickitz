c4f184e44111f5800e759de6d7ebb2a59d6ca3b8
metgag <moreehigh1@gmail.com>
2025-07-30 08:05:49 +0700

    feat: add Login page and import at main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Register from './pages/auth/Register.jsx'
+import Login from './pages/auth/Login.jsx'
 import Movies from './pages/movie/Movies.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Register /> */}
-    <Movies />
+    <Login />
+    {/* <Movies /> */}
   </StrictMode>,
 )
