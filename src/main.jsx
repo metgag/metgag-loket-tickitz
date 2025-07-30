2b467b427ef9e801e7d35b072c0e0280bd3f3290
metgag <moreehigh1@gmail.com>
2025-07-30 07:46:59 +0700

    feat: import Movies page, and ignore .env file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Register from './pages/auth/Register.jsx'
+import Movies from './pages/movie/Movies.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Register />
+    {/* <Register /> */}
+    <Movies />
   </StrictMode>,
 )
