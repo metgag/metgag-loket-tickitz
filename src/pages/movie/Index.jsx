d8d3989fa9499ae321ed7fb4f1ea4e70e526a3ec
metgag <moreehigh1@gmail.com>
2025-08-02 14:06:36 +0700

    refactor: replace Subscription with props , remove MovThumbnail props and replace heading style with variable

--- src/pages/movie/Index.jsx
+++ src/pages/movie/Index.jsx
@@ -1,24 +1,21 @@
 import { useState, useEffect } from 'react'
-// import './global.css'
-// import './index.css'
 import MovieCard from '../../components/MovieCard.jsx'
-import MovThumbnail from '../../components/MovThumbnail.jsx';
-import ChooseItem from '../../components/ChooseItem.jsx';
+import ChooseItem from '../../components/ChooseItem.jsx'
+import Subscription from '../../components/Subscription.jsx'
+import { format } from 'date-fns'
 
 export default function Index() {
   const [movies, setMovies] = useState([]);
-  const [genres, setGenres] = useState([]);
-  const thumbnails = [
-    "/thumbnail/thumb-babayaga.png",
-    "/thumbnail/thumb-lion-king.png",
-    "/thumbnail/thumb-spiderman.png",
-    "/thumbnail/thumb-robloxx.png",
-  ];
   const whyChoose = [
     { title: "Guaranteed", img: "/icon/shield-done.svg" },
     { title: "Affordable", img: "/icon/check-circle.svg" },
     { title: "24/7 Customer Support", img: "/icon/speech-bubble.svg" },
   ];
+  const hBlu = "text-[#1D4ED8] text-lg font-bold";
+  const hBlk = "text-[#121212] text-3xl";
+  const rounded = `rounded-full text-xl size-[2.5rem] bg-[#A0A3BD] 
+    hover:bg-[#1D4ED8] hover:cursor-pointer flex items-center justify-center
+    text-white`;
 
   const apiToken = import.meta.env.VITE_API_TOKEN;
   const urlMovies = import.meta.env.VITE_MOVIES_URL;
@@ -44,11 +41,12 @@ export default function Index() {
 
       const movies = movieResults.map((movie) => {
         const { id, title, release_date, genre_ids, poster_path } = movie;
+        const fDate = format(new Date(release_date), "MMMM yyyy");
         const result = {
           id,
           title,
           poster_path,
-          release_date,
+          fDate
         };
         const genres = genre_ids.map((genreId) => {
           return genresMap.get(genreId);
@@ -58,29 +56,46 @@ export default function Index() {
       });
 
       setMovies(movies);
-      setGenres(genres);
     })();
   }, []);
 
   return (
-    <main className="flex flex-col gap-[6rem]">
-      <section id="hero" className="flex mt-[2.5rem] gap-[2.5rem]">
-        <div className="l-items flex flex-col justify-center gap-[1rem]">
-          <h4 className="text-[#1D4ED8] text-lg">MOVIE TICKET PURCHASES #1 IN INDONESIA</h4>
-          <h1 className="text-4xl text-[#121212]">Experience the Magic of Cinema: Book Your Tickets Today</h1>
-          <p className="text-[#A0A3BD]">Sign up and get the ticket with a lot
-            of discount</p>
+    <main className="flex flex-col px-28 gap-12">
+      <section id="hero" className="flex h-[88vh] items-center">
+        <div className="l-items flex flex-col gap-4 justify-center">
+          <h4 className={hBlu}>MOVIE TICKET PURCHASES #1 IN INDONESIA</h4>
+          <h1 className="text-5xl text-[#121212]">
+            Experience the Magic of Cinema: Book Your Tickets Today
+          </h1>
+          <p className="text-[#A0A3BD]">
+            Sign up and get the ticket with a lot of discount
+          </p>
         </div>
-        <div className="r-items grid w-full h-[28rem] border grid-template-rows-3">
-          {thumbnails.map((thumb, i) => {
-            return <MovThumbnail i={i} img={thumb} />
-          })}
+        <div className="r-items grid w-full h-[28rem] grid-cols-2 gap-4">
+          <div
+            className="bg-[url(/thumbnail/thumb-babayaga.png)] bg-cover 
+              rounded-t-3xl">
+          </div>
+          <div
+            className="bg-[url(/thumbnail/thumb-lion-king.png)] row-span-2 
+              bg-cover rounded-t-3xl">
+          </div>
+          <div
+            className="bg-[url(/thumbnail/thumb-spidermen.png)] row-span-2 
+              bg-cover rounded-b-3xl">
+          </div>
+          <div
+            className="bg-[url(/thumbnail/thumb-robloxx.jpg)]
+              bg-cover rounded-b-3xl">
+          </div>
         </div>
       </section>
 
-      <section id="choose" className="flex flex-col gap-[1.5rem]">
-        <h4 className="text-[#1D4ED8] text-lg">WHY CHOOSE US</h4>
-        <h3 className="text-[#121212] text-xl">Unleashing the Ultimate Movie Experience</h3>
+      <section id="choose" className="flex flex-col gap-6">
+        <h4 className={hBlu}>WHY CHOOSE US</h4>
+        <h3 className={`${hBlk} w-[40%]`}>
+          Unleashing the Ultimate Movie Experience
+        </h3>
         <div id="point" className="flex flex-row gap-6">
           {whyChoose.map((choose, i) => {
             return <ChooseItem i={i} title={choose.title} img={choose.img} />
@@ -88,47 +103,46 @@ export default function Index() {
         </div>
       </section>
 
-      <section id="watch-today" className="flex flex-col gap-[1rem] items-center">
-        <h4 className="text-[#1D4ED8] text-lg">MOVIES</h4>
-        <h3 className="text-[#121212] text-xl">Exciting Movies That Should Be Watched Today</h3>
-        <div className="movies flex gap-[1rem] px-[1rem]">
-          {movies.map((movie, i) => {
-            return <MovieCard i={i} title={movie.title} poster={movie.poster_path} genres={movie.genres} />
+      <section id="watch-today" className="flex flex-col gap-4 items-center">
+        <h4 className={hBlu}>MOVIES</h4>
+        <h3 className={`${hBlk} w-116 text-center`}>
+          Exciting Movies That Should Be Watched Today
+        </h3>
+        <div className="movies flex gap-4 px">
+          {movies.slice(0, 4).map((movie, i) => {
+            return <MovieCard i={i} title={movie.title}
+              poster={movie.poster_path} genres={movie.genres} />
           })}
         </div>
-        <a href="home-dua.html"
-          className="view flex flex-row items-center color-blue underline">
+        <div
+          className={`flex flex-row ${hBlu} mt-8 items-center gap-2 
+            hover:cursor-pointer hover:opacity-[.7]`}>
           <h5>View All</h5><i className="nf nf-fa-arrow_right_long"></i>
-        </a>
+        </div>
       </section>
 
-      <section id="coming-soon" className="flex flex-col gap-[1rem] items-center">
-        <h4 className="text-[#1D4ED8] text-lg">UPCOMING MOVIES</h4>
-        <div className="soon flex items-center justify-between">
-          <h3 className="text-[#121212] text-xl">Exciting Movie Coming Soon</h3>
-          <div className="btn-right flex">
-            <a href="" className="dot">
-              <i className="nf nf-oct-arrow_left"></i></a>
-            <a href="" className="dot">
-              <i className="nf nf-oct-arrow_right"></i></a>
+      <section id="coming-soon" className="flex flex-col gap-4">
+        <h4 className={hBlu}>UPCOMING MOVIES</h4>
+        <div className="soon flex items-center justify-between w-full">
+          <h3 className={hBlk}>Exciting Movie Coming Soon</h3>
+          <div className="btn-right flex gap-1">
+            <div href="" className={rounded}>
+              <i className="nf nf-oct-arrow_left"></i></div>
+            <div href="" className={rounded}>
+              <i className="nf nf-oct-arrow_right"></i></div>
           </div>
         </div>
-        <div className="movies flex gap-[1rem] px-[1rem]">
-          {movies.map((movie, i) => {
-            return <MovieCard i={i} title={movie.title} poster={movie.poster_path} genres={movie.genres} release={movie.release_date} />
+        <div className="movies flex gap-4 self-center">
+          {movies.slice(8, 12).map((movie, i) => {
+            return <MovieCard i={i} title={movie.title}
+              poster={movie.poster_path} genres={movie.genres}
+              release={movie.fDate} />
           })}
         </div>
       </section>
 
-      <section className="bg-blue flex flex-col items-center" id="newsletter">
-        <h1>Subscribe to our newsletter</h1>
-        <form className="flex" action="">
-          <input type="text" placeholder="First name" />
-          <input type="email" placeholder="Email address" />
-          <button>Subscribe Now</button>
-        </form>
-        <div className="circle"></div>
-      </section>
+      <Subscription />
+
     </main>
 
   )
