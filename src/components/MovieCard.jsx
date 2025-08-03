88e196e2e0a31d3851f8991797b7d7cc4b4ef724
metgag <moreehigh1@gmail.com>
2025-08-03 20:34:51 +0700

    style: additional styling at MovieCard components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 function MovieCard(props) {
   return (
-    <div className={`thumbnail thumbnail-${props.i}`}>
+    <div className={`thumbnail thumbnail-${props.i} flex flex-col gap-3 w-[100%]`}>
       <img src={`${import.meta.env.VITE_POSTER_URL}${props.poster}`}
-        alt="" />
-      <div className="detail">
-        <h4>{props.title}</h4>
-        {props.release && <p>{props.release}</p>}
-        <div className="genre">
+        alt="" className="rounded-[6px] w-100% h-max" />
+      <div className="detail flex flex-col gap-2">
+        <h4 className="text-[#14142B] text-xl font-semibold">{props.title}</h4>
+        {props.release && <p className="text-[#1D4ED8] font-semibold">{props.release}</p>}
+        <div className="genre gap-2 flex flex-wrap">
           {props.genres.map((genre, i) => {
-            return <p key={i}>{genre}</p>
+            return <p key={i} className="bg-[#A0A3BD1A] text-sm rounded-[28px] p-1 px-3 text-[#A0A3BD]">{genre}</p>
           })}
         </div>
       </div>
