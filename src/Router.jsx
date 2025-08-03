15c18dd71d5be1ec25110d8729c0d95f37548b98
metgag <moreehigh1@gmail.com>
2025-08-03 20:25:45 +0700

    feat: add Route to MovieDetail, Order, Payment, and Ticket page and move Home page to index

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,25 +3,43 @@ import Register from './pages/auth/Register.jsx'
 import Login from './pages/auth/Login.jsx'
 import Index from './pages/movie/Index.jsx'
 import Movies from './pages/movie/Movies.jsx'
+import MovieDetail from './pages/movie/MovieDetail.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import Order from './pages/payment/Order.jsx'
+import Payment from './pages/payment/Payment.jsx'
+import Ticket from './pages/payment/Ticket.jsx'
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route element={<RouterLayout />}>
-          <Route index element={<Index />} />
-          <Route path="movies" element={<Movies />} />
+        <Route path="auth">
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+
+        <Route index element={(
+          <>
+            <Navbar />
+            <Index />
+            <Footer />
+          </>
+        )} />
+
+        <Route path="movie" element={<RouteLayout />}>
+          <Route path="list" element={<Movies />} />
+          <Route path="order" element={<Order />} />
+          <Route path="payment" element={<Payment />} />
+          <Route path="detail/:movieId" element={<MovieDetail />} />
+          <Route path="ticket" element={<Ticket />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-function RouterLayout() {
+function RouteLayout() {
   return (
     <>
       <Navbar />
