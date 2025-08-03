e8ead2c57fb15fdf0099c8fcc559e734c067eeeb
metgag <moreehigh1@gmail.com>
2025-08-03 20:40:05 +0700

    refactor: replace move page with Router at main

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Register from './pages/auth/Register.jsx'
-// import Login from './pages/auth/Login.jsx'
-// import Movies from './pages/movie/Movies.jsx'
 import Router from './Router'
+import './assets/css/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <Register /> */}
-    {/* <Login /> */}
-    {/* <Movies /> */}
-
     <Router />
-  </StrictMode>,
+  </StrictMode>
 )
