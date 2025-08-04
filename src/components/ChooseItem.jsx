1d1dce2e94bec99e1daf2eced411f6fa10096777
metgag <moreehigh1@gmail.com>
2025-08-04 19:35:09 +0700

    feat: add responsivity below md-screen ChooseItem comp

--- src/components/ChooseItem.jsx
+++ src/components/ChooseItem.jsx
@@ -1,11 +1,13 @@
 function ChooseItem(props) {
   return (
-    <div key={props.i} className="flex gap-2 flex-col">
+    <div key={props.i}
+      className="flex gap-2 items-center md:items-start flex-col"
+    >
       <div className="rounded-full w-[32px] h-[32px] bg-[#1D4ED833] flex justify-center items-center">
         <img className="w-[1rem] h-[1rem]" src={props.img} alt="" />
       </div>
       <h5 className="text-[#18181B] font-semibold">{props.title}</h5>
-      <p className="text-[#A0A3BD] text-justify">Lorem ipsum dolor sit amet, consectetur adipis elit. Sit
+      <p className="text-[#A0A3BD] text-center md:text-justify">Lorem ipsum dolor sit amet, consectetur adipis elit. Sit
         enim nec, proin faucibus nibh et sagittis a. Lacinia
         purus ac amet.</p>
     </div>
