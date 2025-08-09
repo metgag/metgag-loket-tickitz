712586e94d179ba4182268d6582256ff9749fab9
metgag <moreehigh1@gmail.com>
2025-08-09 22:18:55 +0700

    feat: add redux-persist configuration to prevent reset reload

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './Router'
 import './assets/css/styles.css'
-import { store } from './redux/store'
+import store, { persistedStore as persistor } from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Router />
+      <PersistGate loading={null} persistor={persistor}>
+        <Router />
+      </PersistGate>
     </Provider>
   </StrictMode>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage'
+import { PERSIST, persistReducer, REHYDRATE } from "redux-persist";
+
 import authReducer from './slices/authSlice';
 import activeReducer from './slices/currUserSlice'
+import persistStore from "redux-persist/es/persistStore";
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig, 
+  combineReducers({
     auth: authReducer,
     currUser: activeReducer,
-  },
+  }),
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [REHYDRATE, PERSIST],
+      },
+    });
+  }
 });
+
+export const persistedStore = persistStore(store);
+
+export default store;
