81c594a8bd164fa0b7bf7d320639f8663a996848
metgag <moreehigh1@gmail.com>
2025-08-09 09:48:44 +0700

    feat: add Redux store, and provide to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './Router'
 import './assets/css/styles.css'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </StrictMode>
 )
