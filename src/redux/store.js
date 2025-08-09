83a8765f692b7deb5572f34d9849554494a8f099
metgag <moreehigh1@gmail.com>
2025-08-09 21:48:15 +0700

    feat: add auth and currUser reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from './slices/authSlice';
+import activeReducer from './slices/currUserSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+    currUser: activeReducer,
+  },
 });
