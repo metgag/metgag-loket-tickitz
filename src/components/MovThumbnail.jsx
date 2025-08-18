2e2a247e121365d8954548bbfbda05a1d034d687
metgag <moreehigh1@gmail.com>
2025-08-18 16:43:18 +0700

    refactor: rm unused comment and import at MovThumbnail component

--- src/components/MovThumbnail.jsx
+++ src/components/MovThumbnail.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
 function MovThumbnail(props) {
-  // const [span, setSpan] = useState("row-span-1");
-  // props.i % 2 == 0 && setSpan("row-span-2")
-
   return (
     <div key={props.i}
       className={
-        `thumb thumb-${props.i + 1} bg-[url(${props.img})] bg-cover`
+        `thumb thumb-${props.i + 1} ${props.size}  bg-[url(${props.img})] bg-cover`
       }>
     </div>
   )
