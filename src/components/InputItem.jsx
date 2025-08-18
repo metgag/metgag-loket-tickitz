e09d3e17c8c419f9f2a68fde554147e76b3f89ff
metgag <moreehigh1@gmail.com>
2025-08-18 16:38:10 +0700

    refactor: replace value attr to defaultValue InputItem component

--- src/components/InputItem.jsx
+++ src/components/InputItem.jsx
@@ -2,7 +2,7 @@ function InputItem(props) {
   return (
     <div key={props.i} className="flex flex-col gap-[.625rem]">
       <label className="text-[#696F79]" htmlFor={props.id}>{props.label}</label>
-      <input className="w-full p-[.75rem] border-[#DEDEDE] border-1 rounded-[2px] text-[#4E4B66]" value={props.value} type={props.type}
+      <input className="w-full p-[.75rem] border-[#DEDEDE] border-1 rounded-[2px] text-[#4E4B66]" defaultValue={props.value} type={props.type}
         id={props.id} />
     </div>
   )
