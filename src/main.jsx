663c1d5eb3974068c1142eba1cc9ed3c8ab51a95
metgag <moreehigh1@gmail.com>
2025-08-18 16:35:55 +0700

    feat: add context provider Register, Order, and History at main

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,21 @@ import './assets/css/styles.css'
 import store, { persistedStore as persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from "redux-persist/integration/react";
+import RegProvider from './context/users/RegProvider'
+import OrderProvider from './context/order/OrderProvider'
+import HistoryProvider from './context/history/HistoryProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router />
+        <RegProvider>
+          <OrderProvider>
+            <HistoryProvider>
+              <Router />
+            </HistoryProvider>
+          </OrderProvider>
+        </RegProvider>
       </PersistGate>
     </Provider>
   </StrictMode>
