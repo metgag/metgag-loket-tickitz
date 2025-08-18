65e111522c967ddaa60e2382852fe24879379fab
metgag <moreehigh1@gmail.com>
2025-08-18 17:43:29 +0700

    feat: add curr active personal info at Profile Preferences page

--- src/pages/profile/Preferences.jsx
+++ src/pages/profile/Preferences.jsx
@@ -1,8 +1,13 @@
 // import { useSelector } from "react-redux"
+// import { useContext } from 'react';
+import { useSelector } from "react-redux";
+import { orderContext as OrderContext } from "../../context/order/orderContext";
 
 function Preferences() {
   // const user = useSelector((state) => state.currUser.currUser);
-  const whoami = JSON.parse(localStorage.getItem('whoami'));
+  const { email } = useSelector((state) => state.whoami);
+  const { fname, pnumber } = useSelector((state) => state.userInfo);
+  const splitName = fname && fname.split(" ");
 
   return (
     <>
@@ -12,33 +17,42 @@ function Preferences() {
           className="text-[#14142B] font-semibold border-b border-[#DEDEDE] pb-[.75rem] mb-[2rem]">
           Details Information</h5>
         <div className="grid grid-cols-2 gap-[1.5rem] grid-details">
-          <div className="detail flex flex-col gap-[.75rem] detail-1">
-            <label className="text-sm text-[#4E4B66]" htmlFor="">First
+          <div className="frname flex flex-col gap-[.75rem] detail-1">
+            <label className="text-sm text-[#4E4B66]" htmlFor="fname">First
               Name</label>
             <input
+              id="fname"
               className="rounded-lg text-[#4E4B66] p-[.75rem] border border-[#DEDEDE]"
-              type="text" />
+              type="text"
+              defaultValue={splitName ? splitName[0] : ""}
+            />
           </div>
           <div className="detail flex flex-col gap-[.75rem] detail-2">
-            <label className="text-sm text-[#4E4B66]" htmlFor="">Last
+            <label className="text-sm text-[#4E4B66]" htmlFor="lname">Last
               Name</label>
             <input
+              id="lname"
               className="rounded-lg text-[#4E4B66] p-[.75rem] border border-[#DEDEDE]"
-              type="text" />
+              type="text"
+              defaultValue={splitName ? splitName[1] : ""}
+            />
           </div>
           <div className="detail flex flex-col gap-[.75rem] detail-3">
             <label className="text-sm text-[#4E4B66]" htmlFor="">E-mail</label>
             <input
               className="rounded-lg text-[#4E4B66] p-[.75rem] border border-[#DEDEDE]"
-              type="email" name="" id="" defaultValue={whoami.email} />
+              type="email" name="" id="" defaultValue={email} />
           </div>
           <div className="detail flex flex-col gap-[.75rem] detail-4">
-            <label className="text-sm text-[#4E4B66]" htmlFor="">
+            <label className="text-sm text-[#4E4B66]" htmlFor="pnumber">
               Phone Number
             </label>
             <input
+              id="pnumber"
               className="rounded-lg text-[#4E4B66] p-[.75rem] border border-[#DEDEDE]"
-              type="text" />
+              type="text"
+              defaultValue={pnumber}
+            />
           </div>
         </div>
       </div>
