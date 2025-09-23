5357a48459e6cc1edb25e536cae0225176e13bc8
metgag <moreehigh1@gmail.com>
2025-09-23 14:40:21 +0700

    feat: add personalSlice to store user's profile

--- src/redux/slices/personalSlice.js
+++ src/redux/slices/personalSlice.js
@@ -1,23 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  fname: null,
-  pnumber: null
+  profile: {},
+  isLoading: true,
+  isSuccess: false,
+  isFailed: false,
+  error: null,
 };
 
-export const personalSlice = createSlice({
-  name: "userInfo",
-  initialState,
-  reducers: {
-    addInfo: (state, { payload }) => {
-      Object.assign(state, {
-        fname: payload.fname,
-        pnumber: payload.pnumber,
-      });
-    },
-  },
-});
+export const getProfile = createAsyncThunk(
+  "user/get_profile",
+  async (token, { rejectWithValue }) => {
+    try {
+      const url = `${import.meta.env.VITE_BASE_API_URL}/users/`;
+      const options = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
-export const { addInfo } = personalSlice.actions;
+      const resp = await fetch(url, options)
+      if (!resp.ok) throw resp.statusText;
+
+      const data = resp.json()
+      return data.result;
+    } catch (err) {
+      return rejectWithValue(err)
+    }
+  }
+);
+
+const personalSlice = createSlice({
+  name: "whoami",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProfile.pending, (state) => {
+        state.isLoading = true;
+        state.isSuccess = false;
+        state.isFailed = false;
+        state.error = null;
+      })
+      .addCase(getProfile.fulfilled, (state, { payload }) => {
+        state.profile = payload;
+        state.isSuccess = true;
+        state.isLoading = false;
+      })
+      .addCase(getProfile.rejected, (state, { payload, error }) => {
+        state.error = { payload, error };
+        state.isFailed = true;
+        state.isLoading = false;
+      })
+  }
+})
 
 export default personalSlice.reducer;
